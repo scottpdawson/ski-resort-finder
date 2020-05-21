301094fe816197286db7dfa8bbae7b3d81d6ff40
Scott Dawson <github@dawsonmediadesign.com>
2020-05-21 09:38:05 -0400

    adding firebase config with .env reference

---
+++ src/configs/firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "ski-resorts-7ed27.firebaseapp.com",
+    databaseURL: "https://ski-resorts-7ed27.firebaseio.com",
+    projectId: "ski-resorts-7ed27",
+    storageBucket: "ski-resorts-7ed27.appspot.com",
+    messagingSenderId: "230360520955",
+    appId: "1:230360520955:web:e5d88f0dd956c7932d385e",
+    measurementId: "G-STEZ9D4QMZ"
+};
